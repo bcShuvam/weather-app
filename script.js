9966bed0eefb7d693d9f23b3fa0afdfe96bf4fe3
Shuvam BC <81623225+bcShuvam@users.noreply.github.com>
2024-12-29 09:17:23 +0545

    Final commit

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
-const getWeather = async (cityName, countryCode) => {
+const apiKey = "6b66e765bffa2781b5c12a6d1b9b2aab";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const getWeather = async (cityName = "kathmandu", countryCode = "NP") => {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${
-        cityName == "" ? "Biratnagar" : cityName
-      },${
-        countryCode == "" ? "NP" : countryCode
-      }&APPID=6b66e765bffa2781b5c12a6d1b9b2aab`
+      `${apiUrl}${cityName},${countryCode}&APPID=${apiKey}`
     );
+    console.log(cityName, countryCode);
+    console.log(`${apiUrl}${cityName},${countryCode}&APPID=${apiKey}`);
     const data = await response.json();
-    const tempInKelvin = data.main.temp;
+    const tempInKelvin = data.main.temp; // default temp is in kelvin
     const humidity = data.main.humidity;
     const pressure = data.main.pressure;
     const weatherDescription = data.weather[0].description;
+    const weatherIcon = data.weather[0].icon;
     const country = data.sys.country;
     const city = data.name;
     const visibility = (data.visibility / 1000).toFixed(2);
     const sunrise = data.sys.sunrise;
     const sunset = data.sys.sunset;
-    const tempInCelsius = (tempInKelvin - 273.15).toFixed(1);
+    const tempInCelsius = (tempInKelvin - 273.15).toFixed(1); // converting temp in kelvin to celsius
+    const milesToKm = 1.60934;
+    const windSpeed = (data.wind.speed * milesToKm).toFixed(2);
+    const windDirection = data.wind.deg;
+    const myDate = new Date();
+    const currentDate = myDate.toString().slice(0, 24);
+    console.log(windSpeed, windDirection);
     console.log(data);
     console.log(secondsToDateTime(sunrise));
     console.log(secondsToDateTime(sunset));
+    document.getElementById("date-time").innerHTML = `${currentDate}`;
     document.getElementById("temperature").innerHTML = ` ${tempInCelsius}°C`;
     document.getElementById("humidity").innerHTML = ` ${humidity}%`;
     document.getElementById("pressure").innerHTML = ` ${pressure}hPa`;
+    document.getElementById("wind-speed").innerHTML = ` ${windSpeed}km/h`;
+    document.getElementById("wind-direction").innerHTML = ` ${windDirection}°`;
     document.getElementById("visibility").innerHTML = ` ${visibility}km`;
     document.getElementById(
       "weather-description"
     ).innerHTML = `${weatherDescription}`;
+    document.getElementById(
+      "weather-icon"
+    ).src = `https://openweathermap.org/img/w/${weatherIcon}.png`;
     document.getElementById("address").innerHTML = `${city}, ${country
       .toString()
       .toLowerCase()}`;
@@ -37,8 +50,8 @@ const getWeather = async (cityName, countryCode) => {
     document.getElementById("sunset-text").innerHTML = `${secondsToDateTime(
       sunset
     )}`;
-  } catch (error) {
-    console.error(`Error =  ${error}`);
+  } catch (err) {
+    console.error(`Error =  ${err}`);
   }
 };
 
@@ -120,57 +133,7 @@ const secondsToDateTime = (time) => {
   const second = utcDate.getSeconds();
   return `${weekday[day].substring(0, 3)}, ${year}/${
     month + 1
-  }/${date} ${hour}:${minute}:${second}`; // Format to 'YYYY-MM-DD HH:mm:ss'
-  //   let myDate = new Date();
-  //   console.log(myDate);
-  //   console.log(`toString = ${myDate.toString()}`);
-  //   console.log(`toISOString = ${myDate.toISOString()}`);
-  //   console.log(`toLocaleString = ${myDate.toLocaleString()}`);
-  //   console.log(`toLocalDateString = ${myDate.toLocaleDateString()}`);
-  //   console.log(`toLocalTimeString = ${myDate.toLocaleTimeString()}`);
-  //   console.log(`toJson = ${myDate.toJSON()}`);
-  //   console.log(typeof Date.now());
-  //   let myCreatedDate = new Date(2004, 11, 1);
-  //   console.log(myCreatedDate.toDateString());
-  //   console.log(myCreatedDate.toLocaleString());
-  //   let myFormattedDateTime = new Date("2024-12-13");
-  //   console.log(myFormattedDateTime.toDateString());
-  //   console.log(myFormattedDateTime.toLocaleString());
-  //   const utcDate = new Date(timestamp * 1000);
-  // Time stamps
-  //   let myTimeStamps = Date.now();
-  //   console.log(Math.floor(myTimeStamps / 1000));
-  //   console.log(myDate.getTime());
-  //   const weekday = [
-  //     "Sunday",
-  //     "Monday",
-  //     "Tuesday",
-  //     "Wednesday",
-  //     "Thursday",
-  //     "Friday",
-  //     "Saturday",
-  //   ];
-  //   const year = myDate.getFullYear();
-  //   const month = myDate.getMonth();
-  //   const date = myDate.getDate();
-  //   const day = myDate.getDay();
-  //   const hour = myDate.getHours();
-  //   const minute = myDate.getMinutes();
-  //   const second = myDate.getSeconds();
-  //   console.log(
-  //     `${weekday[day]}, ${year}/${month + 1}/${date} ${hour}:${minute}:${second}`
-  //   );
-  //   console.log(
-  //     myDate.toLocaleString("default", {
-  //       hour12: true,
-  //       year: "numeric",
-  //       month: "2-digit",
-  //       day: "2-digit",
-  //       hour: "2-digit",
-  //       minute: "2-digit",
-  //       second: "2-digit",
-  //     })
-  //   );
+  }/${date} ${hour}:${minute}:${second}`; // Format to 'Weekday, YYYY-MM-DD HH:mm:ss'
 };
 
 function searchByCity() {
@@ -178,4 +141,4 @@ function searchByCity() {
   console.log("Hello world");
 }
 
-getWeather("", "");
+getWeather();
